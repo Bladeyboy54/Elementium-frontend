3ca89c7fcd01fe64c6b08e3304fab7c94e7d2754
Godwin Cameron <200109@virtualwindow.co.za>
2024-08-12 19:13:29 +0200

    setup and started frontend for profile

--- elementium/elementium-app/src/App.tsx
+++ elementium/elementium-app/src/App.tsx
@@ -5,12 +5,15 @@ import "./App.css";
 import "./utility/ui/globalStyle.css";
 import Routes from "./utility/global/routes";
 import { AuthProvider } from "./utility/global/auth/authProvider";
+import { User } from "./screens/Account/User";
 
 function App() {
   return (
-    <AuthProvider>
-      <Routes />
-    </AuthProvider>
+    // <AuthProvider>
+    //   <Routes />
+    // </AuthProvider>
+
+    <User />
   );
 }
 
--- elementium/elementium-app/src/screens/Account/User/index.tsx
+++ elementium/elementium-app/src/screens/Account/User/index.tsx
@@ -1,6 +1,38 @@
 import React from "react";
 import "./index.css";
+import { Sidebar } from "../../../elements/Sidebar";
 
 export const User = () => {
-  return <div>User</div>;
+  
+  return (
+    <>
+    <div className="main">
+      <div className="side-bar-section"></div>
+      <div className="content">
+
+        <div className="welcome-title-section">
+          <h1>Welcome, Mr. King</h1>
+          <div className="filler-line"></div>
+        </div>
+
+        <div className="info">
+          <div className="center-section">
+            <div className="plaque">
+              <div className="temp-shimmer"></div>
+              <div className="plaque-info">
+                <p><span className="bold-text">Felix</span> King</p>
+                <h3>5355 **** **** 2546</h3>
+                <span className="plaque-date">08/26</span>
+              </div>
+            </div>
+          </div>
+          <div className="right-section"></div>
+          
+        </div>
+        
+
+      </div>
+    </div>     
+    </>
+  );
 };
