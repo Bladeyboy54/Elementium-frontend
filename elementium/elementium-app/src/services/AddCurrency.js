841c431fadf3840fae921349629d7ed2bde96a68
GodwinCameron <71267628+GodwinCameron@users.noreply.github.com>
2024-08-27 19:41:16 +0200

    setup service for topping-up wallet

---
+++ elementium/elementium-app/src/services/AddCurrency.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+export const addCurrency = async (args) => {
+
+    //example of expected args: userId = 1, amount = 100, currency = "h2"
+
+  let userId, // <-- Gets the user ID from the args
+    amount, // <-- This will be the amount "purchased" by teh user for the top-up
+    currency = args; //<-- This specifies wich currency the user is adding to
+
+
+    // these are just placeholder variables for each currecnyt that we will either add to (depending on the currecny selected) or
+    // keep the same if its another curecny being added to.
+  let balance_h2;
+  let balance_li;
+  let balance_pd;
+  let balance_xe;
+
+  
+  try {
+    const response = await fetch(`http://localhost:5138/api/Account/${userId}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json(); // <-- this is the json data of the user so we can extreact the balances of the currencies
+
+    balance_h2 = data.balance_h2;
+    balance_li = data.balance_li;
+    balance_pd = data.balance_pd;
+    balance_xe = data.balance_xe; //<-- Here we set each of the placeholder variables with the actual balances from the user.
+
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+
+  switch (currency) { // <-- now we set up a switch to determine which currency is going to be tpo-upped
+    case "h2":
+      balance_h2 += amount;
+      break;
+    case "li":
+      balance_li += amount;
+      break;
+    case "pd":
+      balance_pd += amount;
+      break;
+    case "xe":
+      balance_xe += amount;
+      break;
+  }
+
+  const payload = { //<- now the payload can look the same and will contain the old balances of the other currencies and the new balance for the top-upped currency depending on the swtich.
+    balance_h2,
+    balance_li,
+    balance_pd,
+    balance_xe,
+  };
+
+  try { //<-- thsi next try catch will put the new balances into the user account 
+    const response = await put(
+      `http://localhost:5138/api/Account/${userId}`,
+      payload
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return data;
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+};
